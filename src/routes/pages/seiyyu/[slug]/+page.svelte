b101f157136c47c41fde5aae5aabbe90f1dad8c1
rioxiu <richardosinulingga@icloud.com>
2024-04-08 16:31:59 +0700

    feat: add new pages as seiyyu pages

--- src/routes/pages/seiyyu/[slug]/+page.svelte
+++ src/routes/pages/seiyyu/[slug]/+page.svelte
@@ -1,6 +1,46 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+  import { writable } from "svelte/store";
+
   export let data: any = [];
   console.log(data);
+
+  //
+
+  // Buat store writable untuk data paginasi
+  const paginatedVoices = writable([]);
+
+  // Hitung jumlah halaman
+  const itemsPerPage = 10;
+  let currentPage = 1;
+
+  function displayPaginatedVoices() {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = Math.min(startIndex + itemsPerPage, data.voices.length);
+    paginatedVoices.set(data.voices.slice(startIndex, endIndex));
+  }
+
+  // Panggil fungsi displayPaginatedVoices untuk menampilkan data pada halaman pertama
+  onMount(displayPaginatedVoices);
+
+  // Fungsi untuk navigasi ke halaman selanjutnya
+  function nextPage() {
+    if (currentPage < totalPages) {
+      currentPage++;
+      displayPaginatedVoices();
+    }
+  }
+
+  // Fungsi untuk navigasi ke halaman sebelumnya
+  function previousPage() {
+    if (currentPage > 1) {
+      currentPage--;
+      displayPaginatedVoices();
+    }
+  }
+
+  // Hitung total halaman
+  const totalPages = Math.ceil(data.voices.length / itemsPerPage);
 </script>
 
 <nav class="flex justify-between p-5 text-white">
@@ -70,6 +110,31 @@
   </section>
 
   <section class="bg-zinc-900 p-4 mx-44 rounded-md">
-    <span>Character</span>
+    <span class="my-10">Character</span>
+    <div class="my-10 gap-4 grid grid-cols-1">
+      {#each data.voices as voices}
+        <div class="flex justify-between">
+          <div class="flex flex-row gap-2">
+            <img
+              class="rounded-lg w-[159px] h-[112.5px]"
+              src={voices.anime.images.webp.image_url}
+              alt=""
+            />
+            <span>{voices.anime.title}</span>
+          </div>
+          <div class="flex flex-row-reverse gap-2">
+            <img
+              class="rounded-lg w-[159px] h-[112.5px]"
+              src={voices.character.images.jpg.image_url}
+              alt=""
+            />
+            <div class="flex flex-col gap-1 items-end">
+              <span>{voices.character.name}</span>
+              <span>{voices.role}</span>
+            </div>
+          </div>
+        </div>
+      {/each}
+    </div>
   </section>
 </main>
